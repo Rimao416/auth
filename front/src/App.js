55462209861d72acdd72c3ff250ad691ee3a6335
Rimao416 <kayumbaomari27@gmail.com>
2024-12-05 11:14:40 +0100

    01_Init

---
+++ front/src/App.js
@@ -0,0 +1,31 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
+import Login from './features/auth/Login'
+import Welcome from './features/auth/Welcome'
+import RequireAuth from './features/auth/RequireAuth'
+import UsersList from './features/users/UsersList'
+import PersistLogin from './features/auth/PersistLogin'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        {/* protected routes */}
+        <Route element={<PersistLogin />}>
+        <Route element={<RequireAuth />}>
+          <Route path="welcome" element={<Welcome />} />
+          <Route path="userslist" element={<UsersList />} />
+        </Route>
+        </Route>
+
+      </Route>
+    </Routes>
+  )
+}
+
+export default App;
---
+++ front/src/features/auth/Login.js
@@ -0,0 +1,98 @@
+import { useRef, useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
+import { setCredentials } from './authSlice'
+import { useLoginMutation } from './authApiSlice'
+import usePersist from '../../hooks/usePersist'
+
+const Login = () => {
+    const userRef = useRef()
+    const errRef = useRef()
+    const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    const navigate = useNavigate()
+
+    const [login, { isLoading }] = useLoginMutation()
+    const [persist, setPersist] = usePersist()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            console.log("Je me lance les mecs")
+            const {accessToken} = await login({ user, pwd }).unwrap()
+            dispatch(setCredentials({ accessToken}))
+            setUser('')
+            setPwd('')
+            navigate('/welcome')
+        } catch (err) {
+            if (!err?.originalStatus) {
+                // isLoading: true until timeout occurs
+                setErrMsg('No Server Response');
+            } else if (err.originalStatus === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.originalStatus === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
+    }
+
+    const handleUserInput = (e) => setUser(e.target.value)
+
+    const handlePwdInput = (e) => setPwd(e.target.value)
+
+    const content = isLoading ? <h1>Loading...</h1> : (
+        <section className="login">
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+
+            <h1>Employee Login</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    value={user}
+                    onChange={handleUserInput}
+                    autoComplete="off"
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={handlePwdInput}
+                    value={pwd}
+                    required
+                />
+                <button>Sign In</button>
+                <label htmlFor="persist">Trust this device</label>
+                <input
+                    type="checkbox"
+                    id="persist"
+                    onChange={(e) => setPersist(e.target.checked)}
+                    checked={persist}
+                />
+            </form>
+        </section>
+    )
+
+    return content
+}
+export default Login
